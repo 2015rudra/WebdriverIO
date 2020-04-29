f1f3ce46721b0c17e6992e16e146551c48da61b2
Rupesh <rupesh3101.kadam@gmail.com>
2020-04-29 22:13:57 +0530

    Added few other xpaths

--- test/element.js
+++ test/element.js
@@ -6,6 +6,12 @@ describe("Interaction with WebdriverIO", function(){
         const search = $('#twotabsearchtextbox');
         search.setValue('Apple Mac Book');
 
+        browser.pause(5000);
+
+        const label = $('//span[@class="a-size-base a-color-base"]');
+        let text = label.getText();
+        console.log(text);
+
     })
 
 });

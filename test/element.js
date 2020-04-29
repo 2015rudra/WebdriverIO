a5e1fda1407ae74dd3489eccd0e0328282a13195
Rupesh <rupesh3101.kadam@gmail.com>
2020-04-29 22:40:18 +0530

    Added one more it block

--- test/element.js
+++ test/element.js
@@ -8,10 +8,36 @@ describe("Interaction with WebdriverIO", function(){
 
         browser.pause(5000);
 
+    })
+
+    it("Get text values" , function()
+    {
+        browser.url('/')
+        browser.pause(5000);
+
         const label = $('//span[@class="a-size-base a-color-base"]');
         let text = label.getText();
         console.log(text);
 
     })
+
+
+    it("Click on search" , function()
+    {
+        browser.url('/')
+        
+        const search = $('#twotabsearchtextbox');
+        
+        search.setValue('Apple Mac Book');
+
+        browser.pause(5000);
+
+        const button = $('//input[@class="nav-input" and @type="submit"]');
+        button.click();
+        browser.pause(5000);
+
+    })
+
+
 
 });

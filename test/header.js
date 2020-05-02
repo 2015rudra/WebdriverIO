3d4e7f7e39990bcd456bd5208a0f326810efc8c0
Rupesh <rupesh3101.kadam@gmail.com>
2020-05-02 17:15:11 +0530

    Added maximize command

--- test/header.js
+++ test/header.js
@@ -3,6 +3,7 @@ describe("Interaction with WebdriverIO", function(){
     it("get the header text" , function()
     {
         browser.url('https://www.freshworks.com/')
+        browser.maximizeWindow();
         const header = $('h1');
         let text = header.getText();
         console.log(text);

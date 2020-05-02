20b1542d19856435eb53d99ea1bcb9349781a297
Rupesh <rupesh3101.kadam@gmail.com>
2020-05-02 17:15:41 +0530

    Created new Page

---
+++ pages/homepage.js
@@ -0,0 +1,21 @@
+class HomePage
+{
+    get pageHeader()
+    {
+        return $('h1');
+    }
+
+    get subHeader()
+    {
+        return $("//p[contains(text(),'Delight')]");
+    }
+
+    get supportLink()
+    {
+        return $("//li[@class='nav-main-item caps-heading-12 has-sub-menu    '][3]");
+    }
+
+
+}
+
+module.exports = new HomePage();
